e95adc5eb96a289b5dcfd664dcf8bf3f1edcb070
MartinFiori <martinfiori@outlook.com>
2021-09-01 13:10:09 -0300

    subiendo repo

---
+++ clases/clase-1/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+
+let name=prompt("Ingrese su nombre");
+let year=parseInt(prompt("Ahora ingrese el año en que nació para calcular su edad"));
+
+alert(`Buenas tardes ${name}! Usted tiene ${2021-year} años`)
