90f74e412f05cd26ae9aacacdc27646fb6c09052
MartinFiori <martinfiori@outlook.com>
2021-09-01 17:39:28 -0300

    correcciones clase 3

--- clases/clase-3/app.js
+++ clases/clase-3/app.js
@@ -5,7 +5,7 @@
         const button = document.querySelector('#button')
         
         button.addEventListener('click', ()=>{
-            let largo = prompt("Ingrese con qué medida de longitud desea trabajar\nPor ej: cm, m, km, etc...");
+            let medida = prompt("Ingrese con qué medida de longitud desea trabajar\nPor ej: cm, m, km, etc...");
             alert("¿El área de qué figura geométrica desea averiguar?");
 
         while(figura != 5){
@@ -15,30 +15,30 @@
                     base = parseInt(prompt("Ingrese la longitud de la base"))
                     height = parseInt(prompt("Ingrese la longitud de la altura"))
                     result = (base*height)/2
-                    alert(`El área del triángulo es de ${result}${largo}.`)
+                    alert(`El área del triángulo es de ${result.toFixed(2)}${medida}.`)
                     break
                 
                 case 2:
                     lado = parseInt(prompt("Ingrese la longitud de uno de los lados"))
                     result = Math.pow(lado,2)
-                    alert(`El área del cuadrado es de ${result}${largo}.`)
+                    alert(`El área del cuadrado es de ${result.toFixed(2)}${medida}.`)
                     break
                 
                 case 3:
                     radio = parseInt(prompt("Ingrese la longitud del radio"))
                     result = Math.PI*Math.pow(radio,2)
-                    alert(`El área del círculo es de ${result}${largo}.`)
+                    alert(`El área del círculo es de ${result.toFixed(2)}${medida}.`)
                     break
                 
                 case 4:
                     base = parseInt(prompt("Ingrese la longitud de la base"))
                     height = parseInt(prompt("Ingrese la longitud de la altura"))
                     result = (base*height)
-                    alert(`El área del rectángulo es de ${result}${largo}.`)
+                    alert(`El área del rectángulo es de ${result.toFixed(2)}${medida}.`)
                     break
                 
                 default: 
-                    alert("Ingrese una opción válida, por favor.")
+                    alert("Espero haberlo ayudado")
             }
         }
 })
