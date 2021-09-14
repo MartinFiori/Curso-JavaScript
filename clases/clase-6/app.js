2a9f435e8ad023d27da3c8b2c30ed49f2a1817d1
MartinFiori <martinfiori@outlook.com>
2021-09-14 16:37:17 -0300

    cambiando título

--- clases/clase-6/app.js
+++ clases/clase-6/app.js
@@ -1,41 +1,4 @@
 "use strict";
-
-// const frutas = []
-
-// class Fruta {
-//     constructor(producto, cantidad, precio) {
-//         this.producto = producto
-//         this.cantidad = cantidad
-//         this.precio = precio
-//     }
-
-//     info() {
-//         return `De ${producto} tenemos ${this.cantidad} y el valor es de $${precio}x kg`
-//     }
-// }
-
-
-// while (frutas.length < 3) {
-//     let producto = prompt("Ingrese el nombre del producto").toLowerCase()
-//     let cantidad = prompt("Ingrese la cantidad del stock")
-//     let precio = prompt("ingrese el valor del producto en pesos argentinos")
-
-//     if (!isNaN(cantidad) && !isNaN(precio)) {
-//         frutas.push(new Fruta(producto, cantidad, precio))
-//     }
-// }
-
-// const mostrarInventario = () => {
-//     console.log(frutas)
-// }
-
-// mostrarInventario()
-
-
-
-
-
-
 const empleados = []
 
 class Empleado {
